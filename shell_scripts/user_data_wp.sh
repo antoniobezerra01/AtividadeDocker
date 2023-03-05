23cf9b0cafded1d614e130cffcd4e5c189c37d36
Antonio Bezerra <antonio.bezerra1414@gmail.com>
2023-03-05 00:25:20 -0300

    Montagem do EFS

--- shell_scripts/user_data_wp.sh
+++ shell_scripts/user_data_wp.sh
@@ -10,3 +10,8 @@ usermod -a -G docker ec2-user
 # Instalação do docker-compose
 curl -L "https://github.com/docker/compose/releases/download/v2.15.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+
+# Montagem do efs
+mkdir -p /mnt/nfs
+echo "172.31.71.111:/ /mnt/nfs nfs defaults 0 0" >> /etc/fstab
+mount -a
