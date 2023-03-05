600aa17728ee7def8f81cdc0fd96530c35681f01
Antonio Bezerra <antonio.bezerra1414@gmail.com>
2023-03-05 00:11:17 -0300

    Instalação do docker-compose

--- shell_scripts/user_data_wp.sh
+++ shell_scripts/user_data_wp.sh
@@ -6,3 +6,7 @@ yum install docker -y
 systemctl start docker
 systemctl enable docker
 usermod -a -G docker ec2-user
+
+# Instalação do docker-compose
+curl -L "https://github.com/docker/compose/releases/download/v2.15.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
